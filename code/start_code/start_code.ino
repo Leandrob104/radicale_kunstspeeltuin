5b4437dd8022a739bf3346c7cb23ecf1455dfe3a
Leandro <32607@ma-web.nl>
2022-12-13 13:04:34 +0100

    begin code

---
+++ code/start_code/start_code.ino
@@ -0,0 +1,29 @@
+#include <ezLED.h> // ezLED library
+
+#define BUTTON_PIN 7
+
+ezLED led(3);  // create a LED object that attach to pin 3
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  led.loop(); // MUST call the led.loop() function in loop()
+
+  int buttonState = digitalRead(BUTTON_PIN);
+
+  if (buttonState == LOW) {
+    led.turnON();       // turn on immediately
+    //led.turnON(1000); // turn on after 1 second
+  } else {
+    led.turnOFF();       // turn off immediately
+    //led.turnOFF(1000); // turn off after 1 second
+  }
+
+  if (led.getOnOff() == LED_ON)
+    Serial.println("LED is ON");
+  else
+    Serial.println("LED is OFF");
+}
